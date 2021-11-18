7256b47aecc554c5e88dfdad56b52ed0bf620faa
Takunda Madechangu <madechangu.takunda@gmail.com>
2021-11-18 21:34:05 +0200

    Added new routes for web and api

--- backend/src/routes/web.js
+++ backend/src/routes/web.js
@@ -4,6 +4,8 @@ const HomeController = require('../controllers/web/homeController')
 const ProductController = require('../controllers/web/productController')
 const AuthController = require('../controllers/web/authController')
 const SettingsController = require('../controllers/web/settingsController')
+const BlogController = require('../controllers/web/blogController')
+const SEOController = require('../controllers/web/seoController')
 
 /* Home Routes */
 router.get('/', HomeController.home);
@@ -25,13 +27,26 @@ router.post('/auth/forgot-password', AuthController.forgotPassword);
 router.get('/auth/reset-password', AuthController.resetPasswordPage);
 router.post('/auth/reset-password', AuthController.resetPassword);
 
-
 /* Settings Routes */
 router.get('/setting/:id', SettingsController.getProduct);
 router.get('/settings', SettingsController.settings);
 router.get('/edit-setting/:key', SettingsController.edit);
 router.post('/update-setting/:key', SettingsController.update);
 router.post('/delete-setting/:key', SettingsController.delete);
+
+/* Blog Routes */
+router.get('/blog/:id', BlogController.getProduct);
+router.get('/blogs', BlogController.settings);
+router.get('/edit-blog/:id', BlogController.edit);
+router.post('/update-blog/:id', BlogController.update);
+router.post('/delete-blog/:id', BlogController.delete);
+
+/* SEO Routes */
+router.get('/seo/:page_num', SEOController.getProduct);
+router.get('/all-seo', SEOController.settings);
+router.get('/edit-seo/:page_num', SEOController.edit);
+router.post('/update-seo/:page_num', SEOController.update);
+router.post('/delete-seo/:page_num', SEOController.delete);
 
 
 
