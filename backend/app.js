35067292d46d24e65d3f48ec74a794dfb5e03407
Takunda Madechangu <madechangu.takunda@gmail.com>
2021-11-18 21:02:17 +0200

    cleaned app.js,

--- backend/app.js
+++ backend/app.js
@@ -1,43 +1,26 @@
-const createError = require('http-errors');
+
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require('mongoose');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const connectDB = require('./config/db');
+const generalMiddleware = require('./src/middleware/general')
+const apiRoutesMiddleware = require('./src/middleware/apiRoutes')
+const webRoutesMiddleware = require('./src/middleware/webRoutes')
+const errorHandlingMiddleware = require('./src/middleware/errorHandling')
+
+const connectDB = require('./src/config/db');
 require('dotenv').config()
 var app = express();
 
 connectDB()
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev')); //logging development info
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// register middleware
+generalMiddleware(app)
+apiRoutesMiddleware(app)
+webRoutesMiddleware(app)
+errorHandlingMiddleware(app)
 
 module.exports = app;
