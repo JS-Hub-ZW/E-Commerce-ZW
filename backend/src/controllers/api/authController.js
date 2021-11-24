607fa7cc43363bdf29fa7d536d767ad93ae9820b
Takunda Madechangu <madechangu.takunda@gmail.com>
2021-11-24 16:42:30 +0200

    Added login implementation

--- backend/src/controllers/api/authController.js
+++ backend/src/controllers/api/authController.js
@@ -1,6 +1,39 @@
+const User = require('../../models/UserModel')
+
 class AuthController {
     static login = (req, res, next) => {
-        res.send('Login');
+        
+        const { username, password } = req.body;
+
+        User.findOne({ username: username }, (error, user) => {
+            if (user) {
+                bcrypt.compare(password, user.password, (error, same) => {
+                    if (same) { // if passwords match
+                        // store user session, will talk about it later
+                        req.session.user_id = user._id;
+                        req.session.username = user.username;
+                        req.session.logged_id = true;
+                        res.json({
+                            status: 'success',
+                            message: 'Login Successful',
+                        })
+                    }
+                    else {
+                        res.json({
+                            status: 'failed',
+                            message: 'Password mismatch',
+                        })
+                    }
+                })
+            }
+            else {
+                res.json({
+                    status: 'failed',
+                    message: 'User does not exist',
+                })
+            }
+        })
+
     }
 
     static register = (req, res, next) => {
@@ -18,4 +51,4 @@ class AuthController {
 
 }
 
-module.exports=AuthController
+module.exports = AuthController
